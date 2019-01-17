c9d5e59a0f16e7c17131070d5db9ecfa88235826
furrki <furkankaynary@gmail.com>
2019-01-17 14:24:39 -0800

    check if image is ok

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="High Resolution Square Image")
 args = parser.parse_args()
 fname = args.file
+
+img = cv2.imread(fname)
+if img is not None:
+    print("OK img")
